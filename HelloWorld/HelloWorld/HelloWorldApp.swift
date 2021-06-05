a797171d674bd75ae7d69bf9063413bc819ddfc4
GetTrained <85407152+GetTrained@users.noreply.github.com>
2021-06-05 09:22:06 -0700

    Add files via upload

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Keerthana Ediga on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
